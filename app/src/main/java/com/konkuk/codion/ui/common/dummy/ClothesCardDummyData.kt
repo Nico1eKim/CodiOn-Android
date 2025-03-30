dbe2c6256a14a72d6b7be013d181b49b788cf6b3
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-31 00:08:38 +0900

    [refactor]: 옷 카트 더미 데이터 수정 (#6)

--- app/src/main/java/com/konkuk/codion/ui/common/dummy/ClothesCardDummyData.kt
+++ app/src/main/java/com/konkuk/codion/ui/common/dummy/ClothesCardDummyData.kt
@@ -12,17 +12,68 @@ data class ClothesCardDummyData(
     val chipList: List<Int> = listOf()
 ) {
     companion object {
-        val dummyData = ClothesCardDummyData(
-            clothesImg = R.drawable.img_clothes_example,
-            wearCount = 5,
-            isHeartClicked = false,
-            clothesName = "갈색 가디건",
-            clothesPersonalColor = "가을 웜",
-            chipList = listOf(
-                R.string.chip_date,
-                R.string.chip_casual,
-                R.string.chip_travel
+        val dummyData = listOf(
+            ClothesCardDummyData(
+                clothesImg = R.drawable.img_clothes_example,
+                wearCount = 5,
+                isHeartClicked = false,
+                clothesName = "갈색 가디건",
+                clothesPersonalColor = "가을 웜",
+                chipList = listOf(
+                    R.string.chip_date,
+                    R.string.chip_casual,
+                    R.string.chip_travel
+                )
+            ),
+            ClothesCardDummyData(
+                clothesImg = R.drawable.img_clothes_example,
+                wearCount = 5,
+                isHeartClicked = false,
+                clothesName = "갈색 가디건",
+                clothesPersonalColor = "가을 웜",
+                chipList = listOf(
+                    R.string.chip_date,
+                    R.string.chip_casual,
+                    R.string.chip_travel
+                )
+            ),
+            ClothesCardDummyData(
+                clothesImg = R.drawable.img_clothes_example,
+                wearCount = 5,
+                isHeartClicked = false,
+                clothesName = "갈색 가디건",
+                clothesPersonalColor = "가을 웜",
+                chipList = listOf(
+                    R.string.chip_date,
+                    R.string.chip_casual,
+                    R.string.chip_travel
+                )
+            ),
+            ClothesCardDummyData(
+                clothesImg = R.drawable.img_clothes_example,
+                wearCount = 5,
+                isHeartClicked = false,
+                clothesName = "갈색 가디건",
+                clothesPersonalColor = "가을 웜",
+                chipList = listOf(
+                    R.string.chip_date,
+                    R.string.chip_casual,
+                    R.string.chip_travel
+                )
+            ),
+            ClothesCardDummyData(
+                clothesImg = R.drawable.img_clothes_example,
+                wearCount = 5,
+                isHeartClicked = false,
+                clothesName = "갈색 가디건",
+                clothesPersonalColor = "가을 웜",
+                chipList = listOf(
+                    R.string.chip_date,
+                    R.string.chip_casual,
+                    R.string.chip_travel
+                )
             )
         )
+
     }
 }
