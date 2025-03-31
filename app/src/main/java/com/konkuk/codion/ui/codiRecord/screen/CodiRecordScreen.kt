cd904571b4034a2668fd37f6281bddb7996550ae
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-03-31 15:07:04 +0900

    [UI]: 나의 코멘트 화면에 추가 (#6)

--- app/src/main/java/com/konkuk/codion/ui/codiRecord/screen/CodiRecordScreen.kt
+++ app/src/main/java/com/konkuk/codion/ui/codiRecord/screen/CodiRecordScreen.kt
@@ -1,20 +1,34 @@
 package com.konkuk.codion.ui.codiRecord.screen
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.konkuk.codion.R
 import com.konkuk.codion.ui.codiRecord.component.CodiRecordCalendar
-import com.konkuk.codion.ui.codiRecord.dummy.CalendarDay
+import com.konkuk.codion.ui.codiRecord.component.MyComment
+import com.konkuk.codion.ui.codiRecord.component.MyCommentEmptyState
+import com.konkuk.codion.ui.codiRecord.dummy.CodiRecordDummyData
 import com.konkuk.codion.ui.common.dummy.ClothesCardDummyData
 import com.konkuk.codion.ui.home.component.ClothesCardList
 import java.time.LocalDate
 
 @Composable
 fun CodiRecordScreen(modifier: Modifier = Modifier) {
+    val codiRecordMap = CodiRecordDummyData.dummyData.associateBy { it.date }
+
+    var selectedDate by remember { mutableStateOf(LocalDate.now()) }
+    val selectedReview = codiRecordMap[selectedDate]
+
     val today = LocalDate.now()
     //    Scaffold(
     //        topBar = {
@@ -22,20 +36,40 @@ fun CodiRecordScreen(modifier: Modifier = Modifier) {
     //        },
     //    ) { innerPadding ->
 
-    Column(
+    LazyColumn(
         modifier = modifier
 //                .padding(innerPadding)
             .fillMaxSize(),
     ) {
-        CodiRecordCalendar(
-            CalendarDay.dummyData.toMap()
-        )
+        item {
+            CodiRecordCalendar(
+                recordData = codiRecordMap.mapValues { it.value.emotion },
+                onDateSelected = { selectedDate = it }
+            )
+        }
 
-        ClothesCardList(
-            title = stringResource(R.string.wore_clothes, today.month.value, today.dayOfMonth),
-            clothesDataList = ClothesCardDummyData.dummyData,
-            isEditable = true
-        )
+        item {
+            ClothesCardList(
+                title = stringResource(
+                    R.string.wore_clothes,
+                    selectedDate.month.value,
+                    selectedDate.dayOfMonth
+                ),
+                clothesDataList = ClothesCardDummyData.dummyData,
+                isEditable = true
+            )
+        }
+
+        item {
+            Spacer(Modifier.height(24.dp))
+        }
+
+        item {
+            selectedReview?.let {
+                MyComment(record = it)
+            } ?: MyCommentEmptyState()
+        }
+
     }
     //}
 }
