7cab9f86c62337845edf90164304206bffee3ff1
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-01 21:38:40 +0900

    [UI]: 코디 평가 이모티콘 컴포넌트 화면에 적용 (#6)

---
+++ app/src/main/java/com/konkuk/codion/ui/codiRecord/screen/RegisterCodiCommentScreen.kt
@@ -0,0 +1,43 @@
+package com.konkuk.codion.ui.codiRecord.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.codion.R
+import com.konkuk.codion.ui.codiRecord.component.TodayCodiEmotion
+import com.konkuk.codion.ui.common.TopAppBarComponent
+
+@Composable
+fun RegisterCodiCommentScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBarComponent(
+                title = stringResource(R.string.add_comment),
+                leftIcon = null,
+                onLeftClicked = null,
+                rightIcon = null,
+                onRightClicked = null
+            )
+        },
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(top = 24.dp)
+            ) {
+                TodayCodiEmotion()
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RegisterCodiCommentScreenPreview() {
+    RegisterCodiCommentScreen()
+}
