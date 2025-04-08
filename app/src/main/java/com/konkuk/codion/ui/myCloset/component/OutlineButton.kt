0aa2fb7bea5617fdbc67063ce4ca975af3745f69
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-04-09 00:31:44 +0900

    [UI]: 비오는 날 버튼 높이 안맞는거 수정 (#6)

--- app/src/main/java/com/konkuk/codion/ui/myCloset/component/OutlineButton.kt
+++ app/src/main/java/com/konkuk/codion/ui/myCloset/component/OutlineButton.kt
@@ -2,13 +2,14 @@ package com.konkuk.codion.ui.myCloset.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.konkuk.codion.ui.theme.CodiOnTypography
@@ -22,17 +23,17 @@ fun OutlineButton(
     isClicked: Boolean? = false,
     onClick: () -> Unit = {},
 ) {
-    Button(
+    Box(
         modifier = modifier
             .fillMaxWidth()
             .height(40.dp)
-            .background(if (isClicked == true) Gray700 else Gray100, shape = RoundedCornerShape(6.dp))
-            .border(1.dp, Gray700, shape = RoundedCornerShape(6.dp)),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = if (isClicked == true) Gray700 else Gray100,
-            contentColor = if (isClicked == true) Gray100 else Gray700
-        ),
-        onClick = onClick
+            .background(
+                if (isClicked == true) Gray700 else Gray100,
+                shape = RoundedCornerShape(6.dp)
+            )
+            .border(1.dp, Gray700, shape = RoundedCornerShape(6.dp))
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
     ) {
         Text(
             text = label,
