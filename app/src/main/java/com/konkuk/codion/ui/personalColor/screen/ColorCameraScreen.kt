9e34c8c3fc7e38182186ba52f5cdd9fcdd807a3e
Daljyeong <jiyoungm1022@gmail.com>
2025-04-09 15:33:57 +0900

    [UI]: 카메라를 통한 퍼스널 분석 촬영 화면 UI 생성 (#7)

---
+++ app/src/main/java/com/konkuk/codion/ui/personalColor/screen/ColorCameraScreen.kt
@@ -0,0 +1,79 @@
+package com.konkuk.codion.ui.personalColor.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.konkuk.codion.R
+import com.konkuk.codion.ui.common.TopAppBarComponent
+import com.konkuk.codion.ui.common.buttons.SmallButtonComponent
+import com.konkuk.codion.ui.personalColor.component.CustomProgressBar
+import com.konkuk.codion.ui.theme.Gray100
+import com.konkuk.codion.ui.theme.Gray500
+import com.konkuk.codion.ui.theme.Gray700
+
+@Composable
+fun ColorCameraScreen() {
+    // 상단바
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.statusBars.asPaddingValues())  // 상태바 아래부터 컴포넌트가 붙도록 처리
+    ) {
+        TopAppBarComponent(
+            title = stringResource(R.string.camera_personal_color),
+            leftIcon = painterResource(R.drawable.ic_back),
+            onLeftClicked = { },
+            rightIcon = null,
+            onRightClicked = null
+        )
+
+        // TODO: 카메라 화면 가져오기 (서버 측에서 어떻게 제공할지 몰라 우선 영역만 잡아둠)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Gray700),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.width(320.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.weight(1f))
+
+                SmallButtonComponent(
+                    modifier = Modifier.width(200.dp),
+                    containerColor = Gray500,
+                    contentColor = Gray100,
+                    text = stringResource(R.string.analyzing)
+                )
+
+                Spacer(modifier = Modifier.height(14.dp))
+
+                CustomProgressBar(0.6f)
+
+                Spacer(modifier = Modifier.height(54.dp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ColorCameraScreenPreview() {
+    ColorCameraScreen()
+}
