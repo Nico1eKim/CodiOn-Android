e611e10f143a407b9f15182788cad269087c0bc1
Daljyeong <jiyoungm1022@gmail.com>
2025-04-14 00:23:50 +0900

    [feat]: 비밀번호 찾기 화면 입력창에 입력 중인 텍스트 보이게 수정 (#7)

--- app/src/main/java/com/konkuk/codion/ui/onboarding/screen/FindPwdScreen.kt
+++ app/src/main/java/com/konkuk/codion/ui/onboarding/screen/FindPwdScreen.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -29,6 +33,8 @@ import com.konkuk.codion.ui.theme.Gray900
 
 @Composable
 fun FindPwdScreen() {
+    var emailInputTest by remember { mutableStateOf("") }
+
     // 상단바
     Column(
         modifier = Modifier
@@ -66,8 +72,8 @@ fun FindPwdScreen() {
                 label = stringResource(R.string.email),
                 isRequired = false,
                 placeholder = stringResource(R.string.email_ph),
-                inputText = "",
-                onTextChanged = { }
+                inputText = emailInputTest,
+                onTextChanged = { emailInputTest = it }
             )
 
             Spacer(modifier = Modifier.height(24.dp))
